18a197680082336b6f6b237670ac86d00689b82d
adumbidiot <nathaniel.daniel23@outlook.com>
2017-06-04 19:22:53 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ mount -t cifs -o guest //192.168.1.17/music /music
 
 cd cherrymusic
 python3 cherrymusic --update
-ls /.local/share/cherrymusic
+ls /root/.local/share/cherrymusic
 python3 cherrymusic
