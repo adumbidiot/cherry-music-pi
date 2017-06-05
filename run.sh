2a4f7b8a98420c13348e6d37d530cf2b607a5dc2
adumbidiot <nathaniel.daniel23@outlook.com>
2017-06-04 18:19:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 mount -t cifs -o guest //192.168.1.17/music /music
 
-cd cherrymusic && python3 cherrymusic --setup
+#cd cherrymusic && python3 cherrymusic --setup
+nano ./.config/cherrymusic/cherrymusic.conf
